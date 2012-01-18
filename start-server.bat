0e79865db29a16e20b2ec4712dfb12fdf30f0b1e
David Skoog <dwskoog@gmail.com>
2012-01-17 22:49:18 -0500

    Programmatically handle RMI Registry.
    Avoids the non-sense of having to muck around with the rmiregistry
    executable since all of that stuff is hidden from inside the code anyway.
    
    I've exposed the ability to launch on a non-default port, but you should
    be leery about it. Sciore's original form has localhost:1099 assumptions
    baked into it in multiple places. The launcher scripts use the default
    port for RMI Registries.

--- start-server.bat
+++ start-server.bat
@@ -2,7 +2,4 @@
     @CALL mvn clean package
 )
 set CLASSPATH=server\target\server-2.8-SNAPSHOT.jar
-@START /MIN rmiregistry
 @java simpledb.server.Startup %1
-@ECHO Killing the rmiregistry...
-@taskkill /t /f /im rmiregistry.exe
--- start-server.sh
+++ start-server.sh
@@ -2,5 +2,4 @@ if [ ! -e "server/target/server-2.8-SNAPSHOT.jar" ]
 then
   mvn clean package
 fi
-CLASSPATH=server/target/server-2.8-SNAPSHOT.jar rmiregistry &
 java -cp server/target/server-2.8-SNAPSHOT.jar simpledb.server.Startup $1
